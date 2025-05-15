6e63827dcfdd8511fe70c4dd4f514b4731e76738
V1nicius00 <vinicius.cordeiro23_@hotmail.com>
2025-05-15 17:00:54 -0300

    feat: principais expressions languages do JSF

--- src/main/java/dev/vini/learningJSF/bean/estudante/EstudanteRegistrarBean.java
+++ src/main/java/dev/vini/learningJSF/bean/estudante/EstudanteRegistrarBean.java
@@ -1,53 +1,29 @@
-package bean.estudante;
+package dev.vini.learningJSF.bean.estudante;
+
+import dev.vini.learningJSF.model.Estudante;
 
 import javax.inject.Named;
 import java.io.Serializable;
 
 @Named
 public class EstudanteRegistrarBean implements Serializable {
-    private String nome = "Nome";
-    private String sobrenome = "Sobrenome";
-    private double nota1;
-    private double nota2;
-    private double nota3 = 10;
 
-    public String getNome() {
-        return nome;
+    private Estudante estudante = new Estudante();
+    private String[] nomesArray = {"Nome1", "Nome2", "Nome3"};
+
+    public String[] getNomesArray() {
+        return nomesArray;
     }
 
-    public void setNome(String nome) {
-        this.nome = nome;
+    public void setNomesArray(String[] nomesArray) {
+        this.nomesArray = nomesArray;
     }
 
-    public String getSobrenome() {
-        return sobrenome;
+    public Estudante getEstudante() {
+        return estudante;
     }
 
-    public void setSobrenome(String sobrenome) {
-        this.sobrenome = sobrenome;
-    }
-
-    public double getNota1() {
-        return nota1;
-    }
-
-    public void setNota1(double nota1) {
-        this.nota1 = nota1;
-    }
-
-    public double getNota2() {
-        return nota2;
-    }
-
-    public void setNota2(double nota2) {
-        this.nota2 = nota2;
-    }
-
-    public double getNota3() {
-        return nota3;
-    }
-
-    public void setNota3(double nota3) {
-        this.nota3 = nota3;
+    public void setEstudante(Estudante estudante) {
+        this.estudante = estudante;
     }
 }
