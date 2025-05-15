2dee6ff932a354c124c8073c0922834f05e3b989
V1nicius00 <vinicius.cordeiro23_@hotmail.com>
2025-05-15 17:27:21 -0300

    feat: criando funções de redirecionamento entre as páginas

--- src/main/java/dev/vini/learningJSF/bean/estudante/EstudanteRegistrarBean.java
+++ src/main/java/dev/vini/learningJSF/bean/estudante/EstudanteRegistrarBean.java
@@ -4,12 +4,52 @@ import dev.vini.learningJSF.model.Estudante;
 
 import javax.inject.Named;
 import java.io.Serializable;
+import java.util.*;
+
+import static java.util.Arrays.asList;
 
 @Named
 public class EstudanteRegistrarBean implements Serializable {
 
     private Estudante estudante = new Estudante();
-    private String[] nomesArray = {"Nome1", "Nome2", "Nome3"};
+    private String[] nomesArray = {"NomeArray1", "NomeArray2", "NomeArray3"};
+    private List<String> nomesList = asList("NomeList1", "NomeList2", "NomeList3");
+    private Set<String> nomesSet = new HashSet<>(asList("NomeSet1", "NomeSet2", "NomeSet3"));
+    private Map<String, String> nomesMap = new HashMap<>();
+
+    {
+        nomesMap.put("NomeSet1", "SobrenomeSet1");
+        nomesMap.put("NomeSet2", "SobrenomeSet2");
+        nomesMap.put("NomeSet3", "SobrenomeSet3");
+    }
+
+    public String redirectTo(String page){
+        return page+"?faces-redirect=true";
+    }
+
+    public Map<String, String> getNomesMap() {
+        return nomesMap;
+    }
+
+    public void setNomesMap(Map<String, String> nomesMap) {
+        this.nomesMap = nomesMap;
+    }
+
+    public Set<String> getNomesSet() {
+        return nomesSet;
+    }
+
+    public void setNomesSet(Set<String> nomeSet) {
+        this.nomesSet = nomeSet;
+    }
+
+    public List<String> getNomesList() {
+        return nomesList;
+    }
+
+    public void setNomesList(List<String> nomesList) {
+        this.nomesList = nomesList;
+    }
 
     public String[] getNomesArray() {
         return nomesArray;
